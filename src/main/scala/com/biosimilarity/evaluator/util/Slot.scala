f8b14b5241918ac03d046ed31583d7031033e78b
Mike Stay <metaweta@gmail.com>
2013-12-06 16:19:40 -0700

    Added usage object

--- src/main/scala/com/biosimilarity/evaluator/util/Slot.scala
+++ src/main/scala/com/biosimilarity/evaluator/util/Slot.scala
@@ -67,3 +67,18 @@ class Slot(filter: CnxnCtxtLabel[String,String,String], connection: PortableAgen
     )
   }
 }
+
+object Usage {
+  import com.biosimilarity.evaluator.distribution.PortableAgentCnxn
+  import java.net.URI
+  import com.biosimilarity.evaluator.distribution.usage.SimpleClient._
+  
+  def onPut() = println("onPut: done")
+  def onGet[T](value: T) = println("onGet: value = " + value)
+  def onRead[T](value: T) = println("onRead: value = " + value)
+  
+  val slot = new Slot(
+    fromTermString("foo(_)").get,
+    PortableAgentCnxn(new URI("a://b"), "flat", new URI("c://d"))
+  )
+}
